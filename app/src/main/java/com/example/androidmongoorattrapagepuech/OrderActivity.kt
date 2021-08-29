34bd417578aa900586bdd6fbffa0868cf3fffbdc
Antoine Puech <antoine.puech@edu.devinci.fr>
2021-08-29 12:47:28 +0200

    create i,gredient page and form page with OrderActivity class and Ressource file finish the project

--- app/src/main/java/com/example/androidmongoorattrapagepuech/OrderActivity.kt
+++ app/src/main/java/com/example/androidmongoorattrapagepuech/OrderActivity.kt
@@ -1,18 +1,18 @@
 package com.example.androidmongoorattrapagepuech
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
 
-class IngredientActivity : AppCompatActivity() {
+class OrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ingredient)
-        val buttonIngredient = findViewById<View>(R.id.orderbutton) as Button
+        setContentView(R.layout.activity_order)
+        val buttonIngredient = findViewById<View>(R.id.button) as Button
         buttonIngredient.setOnClickListener {
-            val intent = Intent(applicationContext, OrderActivity::class.java)
+            val intent = Intent(applicationContext, MainActivity::class.java)
             startActivity(intent)
         }
     }
